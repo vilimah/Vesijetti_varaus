2048adfd56484c419b4063b82a9d0050f311d35b
vilimah <vilimahonen@icloud.com>
2024-12-01 18:08:31 +0200

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -3,9 +3,65 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password TEXT
 );
+
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     content TEXT,
-    user_id INTEGER REFERENCES users,
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
     sent_at TIMESTAMP
 );
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    price INT,
+    date TEXT,
+    time TEXT,
+    visible BOOLEAN
+);
+
+CREATE TABLE reservations (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
+    prod_id INTEGER REFERENCES products ON DELETE CASCADE,
+    reserved_at TIMESTAMP
+);
+
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    description TEXT
+);
+
+INSERT INTO roles (name, description) VALUES (
+    'user', 'käyttäjä'
+);
+
+INSERT INTO roles (name, description) VALUES (
+    'admin', 'pääkäyttäjä'
+);
+
+CREATE TABLE userRoles (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
+    role_id INTEGER REFERENCES roles ON DELETE CASCADE
+);
+
+CREATE TABLE info (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    created TIMESTAMP
+);
+
+
+INSERT INTO products (title, description, price, date, time, visible) VALUES (
+    'Vesijetti', '2 tuntia vesijeteillä ajelua, tankkaaminen kuuluu hintaan', '149', '30.11.2024','18.30', 'TRUE'
+);
+
+INSERT INTO products (title, description, price, date, time, visible) VALUES (
+    'Vesijetti', '2 tuntia vesijeteillä ajelua, tankkaaminen kuuluu hintaan', '149', '31.11.2024', '14.30', 'TRUE'
+);
+
+INSERT INTO userRoles (user_id, role_id) VALUES ('1', '2');
