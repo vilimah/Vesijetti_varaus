64ba8cfc2a2ce7f5996ae4fde7c3284c4a163b8f
vilimah <vilimahonen@icloud.com>
2024-12-01 18:08:09 +0200

    Add files via upload

--- messages.py
+++ messages.py
@@ -2,12 +2,13 @@ from db import db
 import users
 from sqlalchemy.sql import text
 
-
+# hakee kaikki palautteet listana
 def get_list():
     sql = text("SELECT M.content, U.username, M.sent_at FROM messages M, users U WHERE M.user_id=U.id ORDER BY M.id")
     result = db.session.execute(sql)
     return result.fetchall()
 
+# lähettää palautteen
 def send(content):
     user_id = users.user_id()
     if user_id == 0:
@@ -17,6 +18,7 @@ def send(content):
     db.session.commit()
     return True
 
+# lataa omat palautteet
 def my_questions():
     user_id = users.user_id()
     if user_id == 0:
@@ -25,6 +27,7 @@ def my_questions():
     result = db.session.execute(sql, {"user_id":user_id})
     return result.fetchall()
 
+# oman palautteen poisto
 def delete_own(message_id):
     user_id = users.user_id()
     if user_id == 0:
